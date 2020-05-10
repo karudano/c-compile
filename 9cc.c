f6312feac53060d448bf1b41ce48c1433d70a15b
karudano <nadakai45@gmail.com>
2020-05-10 14:38:59 +0900

    エラーメッセージの改良

--- 9cc.c
+++ 9cc.c
@@ -22,9 +22,16 @@ struct Token {
 
 Token *token;
 
-void error(char *fmt,...){
+char *user_input;
+
+void error_at(char *loc,char *fmt,...){
     va_list ap;
     va_start(ap,fmt);
+
+    int pos = loc - user_input;
+    fprintf(stderr, "%s\n",user_input);
+    fprintf(stderr, "%*s", pos, "");
+    fprintf(stderr,"^ ");
     vfprintf(stderr,fmt,ap);
     fprintf(stderr,"\n");
     exit(1);
@@ -37,12 +44,14 @@ bool consume(char op){
 }
 
 void expect(char op){
-    if (token->kind != TK_RESERVED || token->str[0] != op) error("'%c'ではありません",op);
+    if (token->kind != TK_RESERVED || token->str[0] != op) error_at(token->str,"'%c'ではありません",op);
     token = token->next;
 }
 
 int expect_number(){
-    if (token->kind != TK_NUM) error("数ではありません");
+    if (token->kind != TK_NUM){
+        error_at(token->str,"数ではありません");
+    }
     int val = token->val;
     token = token->next;
     return val;
@@ -82,7 +91,7 @@ Token *tokenize(char *p){
             continue;
         }
 
-        error("トークナイズできません");
+        error_at(p,"トークナイズできません");
     }
     new_token(TK_EOF,cur,p);
     return head.next;
@@ -94,13 +103,14 @@ int main(int argc,char **argv){
         return 1;
     }
 
+    user_input = argv[1];
     token = tokenize(argv[1]);
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    printf("    mov rax, %ld\n", expect_number());
+    printf("    mov rax, %d\n", expect_number());
 
     while (!at_eof()){
         if (consume('+')){
